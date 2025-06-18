286b40813847c13ad375e4c0e0cedab6faa24018
bhargav <jerry@yahoo.com>
2025-06-18 17:39:19 +0530

    check route

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from app import create_app
+# from app import create_app
 
-app = create_app()
+# app = create_app()
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello from Flask!"
+
