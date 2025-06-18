0cfe9f994a3d2687a0aa5ee85606037f595a3f9a
Yogesh <yogesh@codonnier.com>
2025-06-18 14:53:40 +0530

    seccont commit with new code

---
+++ app/routes.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, jsonify, request
+from .services import imgDetect
+import torch
+
+main = Blueprint('main', __name__)
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='C:/xampp/htdocs/yolo_model/posture_tester/best_posture_new_1.pt', force_reload=False)
+
+@main.route('/', methods=['GET'])
+def hello_world():
+    return jsonify({"message": "Hello, World!"})
+
+@main.route('/detect', methods=['POST'])
+def detect():
+    if 'files' not in request.files:
+        return jsonify({'error': 'No files found'}), 400
+
+    img = request.files['files']
+    result = imgDetect(img,model)
+    return jsonify(result)
