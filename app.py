b415093b1c28046cc7399b8b10e54bcbf11a7549
bhargav <jerry@yahoo.com>
2025-06-18 17:19:38 +0530

    test route

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from app import create_app
+# from app import create_app
 
-app = create_app()
+# app = create_app()
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+from flask import Flask
+from app.routes import bp
+
+app = Flask(__name__)
+app.register_blueprint(bp)
+
+@app.route("/")
+def home():
+    return "Flask is working on Render!"
