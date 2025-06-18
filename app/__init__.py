ce7a079a2b4b59a9b79631af13f0af9d0d25e1ce
bhargav <jerry@yahoo.com>
2025-06-18 16:52:45 +0530

    error solved

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from flask_cors import CORS
+import os
 
 def create_app():
-    app = Flask(__name__)
+    # app = Flask(__name__)
+    template_dir = os.path.abspath('templates')  # absolute path from root
+    app = Flask(__name__, template_folder=template_dir)
 
     CORS(app)
 
     from .routes import main
+    # app.register_blueprint(main)
     app.register_blueprint(main, url_prefix="/api")
 
     return app
