00dc19a07247fe04ba60155cfc31813767d986b7
bhargav <jerry@yahoo.com>
2025-06-19 11:37:31 +0530

    yolo error

--- app/routes.py
+++ app/routes.py
@@ -1,21 +1,22 @@
-from flask import Blueprint, jsonify, render_template
+from flask import Blueprint, jsonify, render_template, request
 import torch
 import sys
 from pathlib import Path
+from app.services import imgDetect
 
 main = Blueprint('main', __name__)
-# model = torch.hub.load('ultralytics/yolov5', 'custom', path='best_posture_new_1.pt', force_reload=False)
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='best_posture_new_1.pt', force_reload=False)
 
 
 # Add YOLOv5 local directory to sys.path
 # sys.path.append(str(Path(__file__).resolve().parent.parent / 'yolov5'))
-yolov5_root = Path(__file__).resolve().parent.parent / 'yolov5'
-sys.path.append(str(yolov5_root))
+# yolov5_root = Path(__file__).resolve().parent.parent / 'yolov5'
+# sys.path.append(str(yolov5_root))
 
-from models.common import DetectMultiBackend
-# Load the model
-weights = Path(__file__).resolve().parent.parent / 'best_posture_new_1.pt'
-model = DetectMultiBackend(weights, device='cpu')
+# from models.common import DetectMultiBackend
+# # Load the model
+# weights = Path(__file__).resolve().parent.parent / 'best_posture_new_1.pt'
+# model = DetectMultiBackend(weights, device='cpu')
 
 
 @main.route('/', methods=['GET'])
@@ -23,11 +24,11 @@ def hello_world():
     # return jsonify({'error': 'No files found'})
     return render_template('index.html')
 
-# @main.route('/detect', methods=['POST'])
-# def detect():
-#     if 'files' not in request.files:
-#         return jsonify({'error': 'No files found'}), 400
+@main.route('/detect', methods=['POST'])
+def detect():
+    if 'files' not in request.files:
+        return jsonify({'error': 'No files found'}), 400
 
-#     img = request.files['files']
-#     result = imgDetect(img,model)
-#     return jsonify(result)
+    img = request.files['files']
+    result = imgDetect(img,model)
+    return jsonify(result)
