5489fc1e8f3b0e66d3f9ec41bc432318b343cf47
bhargav <jerry@yahoo.com>
2025-06-19 11:25:13 +0530

    yolo error

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,22 @@
 from flask import Blueprint, jsonify, render_template
 import torch
+import sys
+from pathlib import Path
 
 main = Blueprint('main', __name__)
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='best_posture_new_1.pt', force_reload=False)
+# model = torch.hub.load('ultralytics/yolov5', 'custom', path='best_posture_new_1.pt', force_reload=False)
+
+
+# Add YOLOv5 local directory to sys.path
+# sys.path.append(str(Path(__file__).resolve().parent.parent / 'yolov5'))
+yolov5_root = Path(__file__).resolve().parent.parent / 'yolov5'
+sys.path.append(str(yolov5_root))
+
+from models.common import DetectMultiBackend
+# Load the model
+weights = Path(__file__).resolve().parent.parent / 'best_posture_new_1.pt'
+model = DetectMultiBackend(weights, device='cpu')
+
 
 @main.route('/', methods=['GET'])
 def hello_world():
